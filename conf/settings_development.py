962acfcee7c398219cb958af1b039f64c9aa9255
chenyi <cy18390810647>
2019-06-25 10:31:24 +0800

    111

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'network',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '192.168.1.45',                   # 开发的时候，使用localhost
+        'PASSWORD': 'zorkdata.8888',                        # 你的数据库password
+        'HOST': '192.168.1.157',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 
